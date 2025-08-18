c20a011802959daa884b28050edac11a5566ae82
syafiqbamazruk <syafiqwebsite@gmail.com>
2025-08-18 17:13:14 +0700

    fungsional search

--- assets/js/wa.js
+++ assets/js/wa.js
@@ -16,3 +16,22 @@
       waPopup.style.display = "none";
       localStorage.setItem("waPopupClosed", "true");
   }
+  const waNumber = "6281216700519";
+
+  const hour = new Date().getHours();
+  let greet = "Halo";
+
+  if (hour >= 5 && hour < 11) {
+      greet = "Halo selamat pagi";
+  } else if (hour >= 11 && hour < 15) {
+      greet = "Halo selamat siang";
+  } else if (hour >= 15 && hour < 18) {
+      greet = "Halo selamat sore";
+  } else {
+      greet = "Halo selamat malam";
+  }
+
+  const message = `${greet} ucic, saya mau bertanya seputar ucic.`;
+
+  const waBtn = document.getElementById("waBtn");
+  waBtn.href = `https://wa.me/${waNumber}?text=${encodeURIComponent(message)}`;
